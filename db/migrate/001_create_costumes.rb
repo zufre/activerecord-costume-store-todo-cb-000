f8dae515ddb0027bf81766a492eecbb6fc5ca783
Cedric David <cedric.david@btopenworld.com>
2018-09-17 05:04:19 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@
 # 002_create_costumes.rb becomes class CreateCostumes
 
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
+# In this change method, create columns with the correct names and
 # value types according to the spec
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
