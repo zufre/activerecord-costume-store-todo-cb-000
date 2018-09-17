5edb1a703a1fac1b6bc05331f38f60e3a61d5c93
Cedric David <cedric.david@btopenworld.com>
2018-09-17 05:26:23 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,6 +4,12 @@ class CreateHountedHouses
   def changes
     create_table :hountes_houses do |t|
       t.string :name
-      
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_time
+      t.datetime :closing_time
+      t.text :long_description
   end
 end
