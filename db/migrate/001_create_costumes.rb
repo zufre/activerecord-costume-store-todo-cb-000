1df99f6566b8866b278a6cc97993384a13c1fd5d
Cedric David <cedric.david@btopenworld.com>
2018-09-17 05:06:58 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price
       t.string :size
       t.string :image_url
-      t.timestamp
+      t.timestamps
     end
   end
 end
