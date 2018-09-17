da919970785c0ff575229b9d4cc52732861d6ac1
Cedric David <cedric.david@btopenworld.com>
2018-09-17 05:15:12 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,7 @@
 
 class CreateCostumeStores < ActiveRecord:Migration[4.2]
   def change
-    create_table :costumestores do |t|
+    create_table :costume_stores do |t|
       t.string :name
       t.string :location
       t.integer :number_of_costumes
